59857ee4c281f1b396d274d598029192a35da046
99tevinn <tevinodiwuor@gmail.com>
2025-03-11 15:41:16 +0300

    tev

--- lib/user_profile.ex
+++ lib/user_profile.ex
@@ -10,6 +10,20 @@ defmodule Friendss.UserProfile do
     field :interests, {:array, :string}
   end
 
+  @spec changeset(
+          {map(),
+           %{
+             optional(atom()) =>
+               atom()
+               | {:array | :assoc | :embed | :in | :map | :parameterized | :supertype | :try,
+                  any()}
+           }}
+          | %{
+              :__struct__ => atom() | %{:__changeset__ => any(), optional(any()) => any()},
+              optional(atom()) => any()
+            },
+          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
+        ) :: Ecto.Changeset.t()
   def changeset(profile, attrs) do
      profile
      |> cast(attrs, [:bio, :age, :interests])
