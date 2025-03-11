5b8e0e817c710a2ebe8f7fa61fddf56c2bd78944
99tevinn <tevinodiwuor@gmail.com>
2025-03-11 15:41:49 +0300

    tev

--- lib/user_profile.ex
+++ lib/user_profile.ex
@@ -2,7 +2,7 @@ defmodule Friendss.UserProfile do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key false
+  @primary_key false 
 
   embedded_schema do
     field :bio, :string
@@ -10,20 +10,6 @@ defmodule Friendss.UserProfile do
     field :interests, {:array, :string}
   end
 
-  @spec changeset(
-          {map(),
-           %{
-             optional(atom()) =>
-               atom()
-               | {:array | :assoc | :embed | :in | :map | :parameterized | :supertype | :try,
-                  any()}
-           }}
-          | %{
-              :__struct__ => atom() | %{:__changeset__ => any(), optional(any()) => any()},
-              optional(atom()) => any()
-            },
-          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
-        ) :: Ecto.Changeset.t()
   def changeset(profile, attrs) do
      profile
      |> cast(attrs, [:bio, :age, :interests])
